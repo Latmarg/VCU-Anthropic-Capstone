5722eb6ada9bebe0f0c61c5d267d85017341e2fb
Latmarg <jack.thumma@gmail.com>
2026-09-14 12:06:26 -0400

    Added claimed-vs-actual table

--- src/inventory.py
+++ src/inventory.py
@@ -13,3 +13,20 @@ for f in files:
     print(len(df))
     #Missing rates
     print(df.isna().mean() * 100)
+
+
+df = pandas.read_csv("data/aei_enriched_claude_ai_2025-08-04_to_2025-08-11.csv")
+
+usage = df[(df["geography"] == "country") & (df["variable"] == "usage_pct")]
+print(usage)
+
+usage_sorted = usage.sort_values("value", ascending= False)
+print(usage_sorted.head())
+
+#Value of US comes to be 21.5% which is similar to the stated 21.6 in the article
+
+percap = df[((df["geography"] == "country") & (df["variable"] == "usage_per_capita_index"))]
+print(percap)
+
+percap_sorted = percap.sort_values("value", ascending= False)
+print(percap_sorted.head(10))
